b43525b0f6be9d2d93ea875672bdd9903d7ec92f
Daniel <danieljchang5@gmail.com>
2026-07-22 08:45:57 -0700

    Add: is-anagram - submission-30

---
+++ Data Structures & Algorithms/is-anagram/submission-30.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        checkS = dict()
+        checkT = dict()
+
+        for i in range(len(s)):
+            if s[i] not in checkS:
+                checkS[s[i]] = 1
+            else:
+                checkS[s[i]] += 1
+            if t[i] not in checkT:
+                checkT[t[i]] = 1
+            else:
+                checkT[t[i]] += 1
+        
+        return checkS == checkT
